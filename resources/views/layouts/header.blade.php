91be3a14e8a38b5af9eb8a19c6b9da9b922cd6c7
Zidane <zidanesanjaya81@Gmail.com>
2024-01-17 21:36:18 +0700

    about layouts

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
+    <a href="#" class="sidebar-toggler flex-shrink-0">
+        <i class="fa fa-bars"></i>
+    </a>
+    <div class="navbar-nav align-items-center ms-auto">
+        <div class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                <img class="rounded-circle me-lg-2" src="/dashmin/img/users.png" alt="" style="width: 40px; height: 40px;">
+                <span class="d-none d-lg-inline-flex">User's Name</span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
+                <a href="#" class="dropdown-item">My Profile</a>
+                <a href="#" class="dropdown-item">Settings</a>
+                <a href="#" class="dropdown-item">Log Out</a>
+            </div>
+        </div>
+    </div>
+</nav>
