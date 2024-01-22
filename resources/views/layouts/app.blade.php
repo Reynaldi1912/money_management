3fdc6b088ff1294e8c05187ed6fe86a95e2f351b
Zidane <zidanesanjaya81@Gmail.com>
2024-01-22 19:52:14 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,6 @@
         @include('layouts.footer')
         <!-- Footer End -->
 
-
-    <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
-    </div>
-
     @include('layouts.js')
 </body>
 
