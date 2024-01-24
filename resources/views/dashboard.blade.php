90bff5ad0544fe30c6a13af7f7230a222248cf51
Zidane <zidanesanjaya81@Gmail.com>
2024-01-24 20:04:09 +0700

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -372,4 +372,43 @@
     </div>
 </div>
 <!-- End::Modal Edit Pemasukan/Pengeluaran -->
+
+<!-- Begin::Modal My Profil -->
+<div class="modal fade" id="myProfile" tabindex="-1" aria-labelledby="myProfileLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">My Profile</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="floatingNamaLengkap" placeholder="" value="Zidane Putra Sanjaya">
+                            <label for="floatingNamaLengkap">Nama Lengkap</label>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-floating mb-3">
+                            <input type="username" class="form-control" id="floatingUsername" placeholder="" value="zidanesanjaya">
+                            <label for="floatingUsername">Username</label>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" id="floatingPassword" placeholder="" value="lumajang">
+                            <label for="floatingPassword">Password</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End::Modal My Profil -->
 @endsection('content')
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
                 <img class="rounded-circle me-lg-2" src="/dashmin/img/users.png" alt="" style="width: 40px; height: 40px;">
                 <span class="d-none d-lg-inline-flex">User's Name</span>
             </a>
-            <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
+            <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0" data-bs-toggle="modal" data-bs-target="#myProfile">
                 <a href="#" class="dropdown-item">My Profile</a>
                 <a href="{{ route('logout') }}" class="dropdown-item text-logout" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     Log Out
